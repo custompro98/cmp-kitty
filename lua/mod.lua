bc38b3d1200db0a96a3637528d523fa489f4dd23
custompro98 <mitchjoa@gmail.com>
2022-05-11 10:55:20 -0400

    Add descriptions for kitty mod operators

--- lua/mod.lua
+++ lua/mod.lua
@@ -1,12 +1,18 @@
 local mod = {
-  ctrl = { detail = "" },
-  alt = { detail = "" },
-  shift = { detail = "" },
-  cmd = { detail = "" },
-  super = { detail = "" },
+  ctrl = { detail = "The ctrl key on your keyboard." },
+  alt = { detail = "The alt/option key on your keyboard." },
+  shift = { detail = "The shift key on your keyboard." },
+  cmd = { detail = "The command key on your keyboard." },
+  super = { detail = "The super key on your keyboard." },
   hyper = { detail = "" },
   meta = { detail = "" },
-  kitty_mod = { detail = "" },
+  kitty_mod = {
+    detail = [[
+E.g. kitty_mod ctrl+shift
+
+The value of kitty_mod is used as the modifier for all default shortcuts, you can change it in your kitty.conf to change the modifiers for all the default shortcuts.
+]]
+  },
 }
 
 return mod
